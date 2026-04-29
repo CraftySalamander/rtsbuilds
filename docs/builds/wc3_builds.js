df97397414294a708dab6545945e33e5e0c38dcc
CraftySalamander <nico.vandernoot+1@gmail.com>
2026-04-29 07:05:41 +0200

    add wc3 bos from pdalcastel

--- docs/builds/wc3_builds.js
+++ docs/builds/wc3_builds.js
@@ -64,5 +64,188 @@ const wc3_build_orders = [
     ]
   },
   "opponent_faction": "Any"
+},
+  {
+  "name": "Orc Beginner Guide with FS HH SH",
+  "faction": "Orcs",
+  "author": "Grubby",
+  "content": {
+    "name": "Orc Beginner Guide with FS HH SH",
+    "race": "Orcs",
+    "opponent_race": "Any",
+    "author": "Grubby",
+    "written_by": "pdalcastel",
+    "build_order": [
+      {
+        "time": "00:00",
+        "food": 5,
+        "notes": [
+          "Train 2@orc_unit/peon.webp@, 4@orc_unit/peon.webp@&gt;@resource/gold.webp@",
+          "1@orc_unit/peon.webp@&gt;@orc_building/altar_of_storms.webp@&gt;@resource/lumber.webp@",
+          "Rally 1st @orc_unit/peon.webp@&gt;@orc_building/orc_burrow.webp@, train @orc_unit/peon.webp@",
+          "Rally 2nd @orc_unit/peon.webp@&gt;@resource/gold.webp@ (5/5)",
+          "Rally 3rd @orc_unit/peon.webp@&gt;@orc_building/war_mill.webp@&gt;@resource/lumber.webp@",
+          "Rally 4th @orc_unit/peon.webp@&gt;@orc_building/barracks.webp@&gt;@resource/lumber.webp@"
+        ]
+      },
+      {
+        "time": "01:08",
+        "food": 9,
+        "notes": [
+          "Train @orc_hero/far_seer.webp@",
+          "3@orc_unit/peon.webp@&gt;@resource/lumber.webp@",
+          "Build @orc_building/orc_burrow.webp@",
+          "Train 2@orc_unit/troll_headhunter.webp@"
+        ]
+      },
+      {
+        "time": "02:45",
+        "food": 21,
+        "notes": [
+          "@orc_building/great_hall.webp@ Tier 2",
+          "Train 4@orc_unit/troll_headhunter.webp@ (6 total)",
+          "Build @orc_building/orc_burrow.webp@@orc_building/voodoo_lounge.webp@"
+        ]
+      },
+      {
+        "time": "05:06",
+        "food": 29,
+        "notes": [
+          "Train @orc_hero/shadow_hunter.webp@",
+          "Buy @orc_items/healing_salve.webp@@orc_items/lesser_clarity_potion.webp@@orc_items/scroll_of_haste.webp@"
+        ]
+      },
+      {
+        "time": "05:22",
+        "food": 34,
+        "notes": [
+          "@orc_building/great_hall.webp@ Tier 3",
+          "Build @orc_building/beastiary.webp@",
+          "Research @orc_upgrades/regenerate.webp@",
+          "Train 1@orc_unit/troll_headhunter.webp@ (7 total)"
+        ]
+      },
+      {
+        "time": "06:58",
+        "food": 36,
+        "notes": [
+          "Train @orc_unit/kodo_beast.webp@",
+          "Build @orc_building/orc_burrow.webp@",
+          "Research @orc_upgrades/head_hunter_berserker.webp@@orc_upgrades/drum.webp@"
+        ]
+      },
+      {
+        "time": "08:00",
+        "food": 40,
+        "notes": [
+          "Continuation:",
+          "2@orc_items/orb_of_lightning.webp@",
+          "@orc_upgrades/steel_armor.webp@@orc_upgrades/steel_ranged.webp@@orc_upgrades/orc_melee_up_one.webp@",
+          "@orc_unit/tauren.webp@@orc_unit/spirit_walker.webp@@orc_unit/raider.webp@",
+          "Tiny @orc_building/great_hall.webp@ expand 5@orc_unit/peon.webp@",
+          "Buy Shredder when expand"
+        ]
+      }
+    ]
+  },
+  "opponent_faction": "Any"
+},
+  {
+  "name": "Orc Bloody Beginner FS Grunts",
+  "faction": "Orcs",
+  "author": "Grubby",
+  "content": {
+    "name": "Orc Bloody Beginner FS Grunts",
+    "race": "Orcs",
+    "opponent_race": "Any",
+    "author": "Grubby",
+    "written_by": "pdalcastel",
+    "build_order": [
+      {
+        "time": "00:00",
+        "food": 5,
+        "notes": [
+          "Train 2@orc_unit/peon.webp@, 4@orc_unit/peon.webp@&gt;@resource/gold.webp@, ",
+          "1@orc_unit/peon.webp@&gt;@orc_building/altar_of_storms.webp@&gt;@resource/lumber.webp@",
+          "Rally 1st @orc_unit/peon.webp@&gt;@orc_building/orc_burrow.webp@&gt;@resource/lumber.webp@",
+          "Rally 2nd @orc_unit/peon.webp@&gt;@orc_building/barracks.webp@&gt;@resource/lumber.webp@",
+          "Rally 3rd @orc_unit/peon.webp@&gt;@resource/gold.webp@"
+        ]
+      },
+      {
+        "time": "01:08",
+        "food": 10,
+        "notes": [
+          "Train @orc_hero/far_seer.webp@",
+          "Build @orc_building/orc_burrow.webp@ and pause @orc_unit/peon.webp@",
+          "Train @orc_unit/grunt.webp@",
+          "Build @orc_building/voodoo_lounge.webp@, resume @orc_unit/peon.webp@",
+          "Train @orc_unit/grunt.webp@"
+        ]
+      },
+      {
+        "time": "02:45",
+        "food": 23,
+        "notes": [
+          "@orc_building/great_hall.webp@ Tier 2",
+          "Buy @orc_items/healing_salve.webp@@orc_items/scroll_of_haste.webp@",
+          "Train 2@orc_unit/grunt.webp@",
+          "Build @orc_building/orc_burrow.webp@"
+        ]
+      },
+      {
+        "time": "05:20",
+        "food": 29,
+        "notes": [
+          "Train @orc_hero/shadow_hunter.webp@",
+          "Build@orc_building/beastiary.webp@@orc_building/tauren_totem.webp@"
+        ]
+      },
+      {
+        "time": "06:00",
+        "food": 34,
+        "notes": [
+          "Train 1@orc_unit/raider.webp@1@orc_unit/spirit_walker.webp@",
+          "Build @orc_building/orc_burrow.webp@"
+        ]
+      },
+      {
+        "time": "6:30",
+        "food": 40,
+        "notes": [
+          "Research @orc_upgrades/ensnare.webp@@orc_upgrades/spirit_walker_adept_training.webp@"
+        ]
+      },
+      {
+        "time": "7:00",
+        "food": 40,
+        "notes": [
+          "Train 1@orc_unit/raider.webp@",
+          "Build @orc_building/war_mill.webp@",
+          "Train 1@orc_unit/spirit_walker.webp@1@orc_unit/kodo_beast.webp@"
+        ]
+      },
+      {
+        "time": "08:00",
+        "food": 50,
+        "notes": [
+          "Build @orc_building/orc_burrow.webp@",
+          "Research @orc_upgrades/steel_armor.webp@"
+        ]
+      },
+      {
+        "time": "08:30",
+        "food": 50,
+        "notes": [
+          "Follow up:",
+          "Replace dead @orc_unit/grunt.webp@ with @orc_unit/troll_headhunter.webp@",
+          "Attack at 50 or 60@resource/food.webp@",
+          "Tiny @orc_building/great_hall.webp@ expand",
+          "@orc_building/great_hall.webp@ Tier 3 for @orc_hero/tauren_chieftain.webp@"
+        ]
+      }
+    ]
+  },
+  "opponent_faction": "Any"
 }
 ];
